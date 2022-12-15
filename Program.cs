95ea1142f48fe88d22c2ea518fa2315ac604ab9b
Patricia Takehara <paty.takeharab@outlook.com>
2022-12-15 16:12:56 -0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿(int Id, string Nome, string Sobrenome, decimal Altura) tupla = (1, "Patricia", "Takehara", 1.53M);
+
+//ValueTuple<int, string, string, decimal> outroExemploTupla = (1, "Patricia", "Takehara", 1.53M);
+//var outroExemploTuplaCreate = Tuple.Create(1, "Patricia", "Takehara", 1.53M);
+
+Console.WriteLine($"Id: {tupla.Id}");
+Console.WriteLine($"Nome: {tupla.Nome}");
+Console.WriteLine($"Sobrenome: {tupla.Sobrenome}");
+Console.WriteLine($"Altura: {tupla.Altura}");
